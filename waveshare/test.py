e3ad999ef47ec0fa6d62692c2254f5be8faa7bc1
BokWeiHong <bokweihong@gmail.com>
2026-02-14 00:01:02 +0800

    changed waveshare code for black and white screen

--- waveshare/test.py
+++ waveshare/test.py
@@ -2,14 +2,14 @@ import sys
 import os
 import socket
 import subprocess 
-from waveshare_epd import epd2in13b_V4
+from waveshare_epd import epd2in13_V4
 from PIL import Image, ImageDraw, ImageFont
 import time
 from datetime import datetime
 
 # --- Configuration ---
-CUSTOM_WIDTH  = 105   
-CUSTOM_HEIGHT = 80
+CUSTOM_WIDTH  = 100 
+CUSTOM_HEIGHT = 75
 
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -38,72 +38,76 @@ def get_system_state():
     time_str = datetime.now().strftime("%H:%M")
     return ip, gps, alfa, time_str
 
-# --- DISPLAY FUNCTION ---
-def update_display(epd, font, img_raw, state):
-    ip, gps_ok, alfa_ok, time_str = state
-    
-    print(f"Refreshing screen for {time_str}...")
-    epd.init()
-
-    canvas_black = Image.new('1', (epd.height, epd.width), 255)
-    canvas_red   = Image.new('1', (epd.height, epd.width), 255)
-    
-    draw_black   = ImageDraw.Draw(canvas_black)
-
-    if img_raw:
-        canvas_black.paste(img_raw, (5, 20), mask=img_raw)
-
-    draw_black.text((5, 5), f"IP:{ip}", font=font, fill=0)
-    draw_black.text((125, 5), f"Time:{time_str}", font=font, fill=0)
-
-    draw_black.text((125, 25), "Hello, \nGloopie here!", font=font, fill=0)
-    
-    draw_black.text((5, 115), "GPS:", font=font, fill=0)
-    if gps_ok:
-        draw_black.text((35, 115), "[OK]", font=font, fill=0)
-    else:
-
-        draw_black.text((35, 115), "!MISSING!", font=font, fill=0)
-
-    draw_black.text((125, 115), "Alfa:", font=font, fill=0)
-    if alfa_ok:
-        draw_black.text((160, 115), "[OK]", font=font, fill=0)
-    else:
-        draw_black.text((160, 115), "!MISSING!", font=font, fill=0)
-
-    draw_black.line((0, 15, epd.height, 15), fill=0, width=1)
-    draw_black.line((0, 110, epd.height, 110), fill=0, width=1)
-
-    epd.display(epd.getbuffer(canvas_black), epd.getbuffer(canvas_red))
-    epd.sleep()
-
 try:
-    epd = epd2in13b_V4.EPD()
-
+    epd = epd2in13_V4.EPD()
+    
     try:
         font = ImageFont.truetype('/home/pi/GloopieGuardian/static/fonts/PressStart2P-Regular.ttf', 6)
     except IOError:
+        print("Custom font not found. Using default.")
         font = ImageFont.load_default()
 
     try:
         img_raw = Image.open('img/gg.png').convert('RGBA')
         img_raw = img_raw.resize((CUSTOM_WIDTH, CUSTOM_HEIGHT), resample=Image.NEAREST)
     except IOError:
+        print("Image 'img/gg.png' not found. Skipping image.")
         img_raw = None
 
-    print("System Monitor Running... (Black & White Mode)")
+    print("Initializing screen...")
+    epd.init()
+    epd.Clear(0xFF)
+
+    base_canvas = Image.new('1', (epd.height, epd.width), 255)
+    draw_base = ImageDraw.Draw(base_canvas)
+
+    if img_raw:
+        base_canvas.paste(img_raw, (5, 25), mask=img_raw)
     
+    draw_base.text((125, 25), "Hello, \nGloopie here!", font=font, fill=0)
+    draw_base.text((5, 115), "GPS:", font=font, fill=0)
+    draw_base.text((125, 115), "Alfa:", font=font, fill=0)
+    draw_base.line((0, 15, epd.height, 15), fill=0, width=1)
+    draw_base.line((0, 110, epd.height, 110), fill=0, width=1)
+
+    epd.displayPartBaseImage(epd.getbuffer(base_canvas.rotate(90, expand=True)))
+
+    print("System Monitor Running... (Partial Refresh Active)")
     last_state = None 
 
     while True:
         current_state = get_system_state()
 
         if current_state != last_state:
-            update_display(epd, font, img_raw, current_state)
+            ip, gps_ok, alfa_ok, time_str = current_state
+
+            dynamic_canvas = base_canvas.copy()
+            draw_dynamic = ImageDraw.Draw(dynamic_canvas)
+
+            draw_dynamic.text((5, 5), f"IP:{ip}", font=font, fill=0)
+            draw_dynamic.text((125, 5), f"Time:{time_str}", font=font, fill=0)
+
+            if gps_ok:
+                draw_dynamic.text((35, 115), "[OK]", font=font, fill=0)
+            else:
+                draw_dynamic.text((35, 115), "!ERR!", font=font, fill=0)
+
+            if alfa_ok:
+                draw_dynamic.text((160, 115), "[OK]", font=font, fill=0)
+            else:
+                draw_dynamic.text((160, 115), "!ERR!", font=font, fill=0)
+
+            epd.displayPartial(epd.getbuffer(dynamic_canvas.rotate(90, expand=True)))
+            
             last_state = current_state
-        
+            print(f"Screen updated at {time_str}")
+
         time.sleep(1)
 
 except KeyboardInterrupt:    
-    epd2in13b_V4.EPD().module_exit()
-    exit()
+    print("\nExiting and clearing screen...")
+    epd.init()
+    epd.Clear(0xFF) 
+    epd.sleep()
+    epd2in13_V4.EPD().module_exit()
+    sys.exit()
